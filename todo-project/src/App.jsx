cad57f4c5f0c30c657984cdc253f230d61a25982
Gerald Morales <85576040+geraldmrls@users.noreply.github.com>
2026-04-27 21:36:07 -0600

    Create async function fetchTodo to test the api call

--- todo-project/src/App.jsx
+++ todo-project/src/App.jsx
@@ -1,24 +1,49 @@
 
 import dayjs from 'dayjs';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Header } from './components/Header/Header.jsx'
 import { TodoBody } from './components/SearchBar/AddTodoInput.jsx';
 import './App.css'
 
+// supabse
+import { supabase } from './supabase.js';
+
 function App() {
 
-const [todo, setTodo] = useState(() => {
+  // supabase test
+  const [todoData, setTodoData] = useState([])
+  useEffect(() => {
+    const fetchTodo = async () => {
+      try {
+        const { data, error } = await supabase
+          .from('todo')
+          .select()
+        if (error) {
+          console.log(error)
+        }
+        setTodoData(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchTodo()
+  }, [])
+
+  console.log(todoData)
+  /////////////////////--supabse test
+
+  const [todo, setTodo] = useState(() => {
     const savedTodos = localStorage.getItem("todos");
     if (!savedTodos) return [];
 
     const parsed = JSON.parse(savedTodos);
 
     return parsed.map(t => ({
-        ...t,
-        isNew: false,
-        createdAt: t.createdAt || dayjs().toISOString()
+      ...t,
+      isNew: false,
+      createdAt: t.createdAt || dayjs().toISOString()
     }));
-});
+  });
 
 
   function getTasksCompleted() {
